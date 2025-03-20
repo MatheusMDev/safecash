335e51eed492193597bdfdb9db3f87d286b3d768
iagodev <lagoaqlagoai@hotmail.com>
2025-03-20 02:31:24 -0300

    criei 3 telas, vao se foder

---
+++ lib/screens/register.dart
@@ -0,0 +1,404 @@
+import 'package:bank_app/screens/face_recognition.dart';
+import 'package:bank_app/screens/home.dart';
+import 'package:bank_app/screens/login.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class RegisterScreen extends StatefulWidget {
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(22, 22, 34, 1),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                );
+              },
+              child: Container(
+                width: 52,
+                height: 52,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: const Color.fromARGB(255, 30, 30, 45),
+                ),
+                child: Center(
+                  child: Transform.translate(
+                    offset: Offset(5, 0),
+                    child: Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 75),
+            Text(
+              'Cadastrar-se',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 38,
+                fontFamily: 'Poppins',
+              ),
+            ),
+            SizedBox(height: 40),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'CPF',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: 'Poppins',
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  cursorColor: Colors.white,
+                  keyboardType: TextInputType.text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Poppins',
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Digite seu CPF',
+                    hintStyle: const TextStyle(
+                      color: Colors.grey,
+                      fontFamily: 'Poppins',
+                    ),
+                    prefixIcon: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.solidIdCard,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                        SizedBox(width: 10),
+                      ],
+                    ),
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 1.0,
+              color: const Color.fromARGB(38, 158, 158, 158),
+              margin: EdgeInsets.only(top: 5.0),
+            ),
+            SizedBox(height: 40),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Nome Completo',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: 'Poppins',
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  keyboardType: TextInputType.text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Poppins',
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Digite seu nome completo',
+                    hintStyle: const TextStyle(
+                      color: Colors.grey,
+                      fontFamily: 'Poppins',
+                    ),
+                    prefixIcon: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          Icons.person_outlined,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                      ],
+                    ),
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 1.0,
+              color: const Color.fromARGB(38, 158, 158, 158),
+              margin: EdgeInsets.only(top: 5.0),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 40),
+                Text(
+                  'Telefone',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: 'Poppins',
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  cursorColor: Colors.white,
+                  keyboardType: TextInputType.text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Poppins',
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Digite seu Telefone',
+                    hintStyle: const TextStyle(
+                      color: Colors.grey,
+                      fontFamily: 'Poppins',
+                    ),
+                    prefixIcon: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          Icons.phone,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                        SizedBox(width: 10),
+                      ],
+                    ),
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 1.0,
+              color: const Color.fromARGB(38, 158, 158, 158),
+              margin: EdgeInsets.only(top: 5.0),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 40),
+                Text(
+                  'E-mail',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: 'Poppins',
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  cursorColor: Colors.white,
+                  keyboardType: TextInputType.text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Poppins',
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Digite seu E-mail',
+                    hintStyle: const TextStyle(
+                      color: Colors.grey,
+                      fontFamily: 'Poppins',
+                    ),
+                    prefixIcon: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          Icons.mail_outlined,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                        SizedBox(width: 10),
+                      ],
+                    ),
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 1.0,
+              color: const Color.fromARGB(38, 158, 158, 158),
+              margin: EdgeInsets.only(top: 5.0),
+            ),
+            SizedBox(height: 40),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Senha',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: 'Poppins',
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  cursorColor: Colors.white,
+                  obscureText: _obscureText,
+                  keyboardType: TextInputType.text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Poppins',
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Digite sua senha',
+                    hintStyle: const TextStyle(
+                      color: Colors.grey,
+                      fontFamily: 'Poppins',
+                    ),
+                    prefixIcon: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          Icons.lock_outline,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                        SizedBox(width: 10),
+                      ],
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscureText
+                            ? Icons.remove_red_eye_outlined
+                            : Icons.visibility,
+                        color: Colors.grey,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                    ),
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 1.0,
+              color: const Color.fromARGB(38, 158, 158, 158),
+              margin: EdgeInsets.only(top: 5.0),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 45,
+                  width: 300,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromRGBO(0, 102, 255, 1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => FaceRecognitionScreen(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Cadastrar',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'Já tem uma conta?',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                    SizedBox(height: 3),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginScreen(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        'Entrar',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 14,
+                          fontFamily: 'Poppins',
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
