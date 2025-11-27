974bcfeb68560a6c32d6b86062f0f1d73bde903c
MatheusMDev <matheu.miranda@hotmail.com>
2025-11-27 11:08:46 -0300

    idtoken

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -2,27 +2,41 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:firebase_auth/firebase_auth.dart';
 
-Future<String?> getIdToken() async { //aqui é um verify
-  try {
-    User? user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      // Obtém o idToken do usuário logado
-      String? idToken = await user.getIdToken();
-      return idToken; 
-    } else {
-      print('Nenhum usuário logado');
-      return null;
-    }
-  } catch (e) {
-    print('Erro ao obter idToken: $e');
+// Função para realizar o login com email e senha
+Future<String?> loginWithEmailAndPassword(String email, String password) async {
+  // URL do Firebase Authentication para autenticar com email e senha
+  final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDXMaZaGsPa3ajobec1c9FK0G-yCqSTMDE');
+
+  // Requisição POST para autenticar com o email e senha
+  final response = await http.post(
+    url,
+    body: json.encode({
+      'email': email,  // Aqui você passa o email real
+      'password': password,  // Senha do usuário
+      'returnSecureToken': true,  // Garantir que o token seja retornado
+    }),
+    headers: {
+      'Content-Type': 'application/json',  // Definir o tipo de conteúdo
+    },
+  );
+
+  if (response.statusCode == 200) {
+    // Se a autenticação for bem-sucedida, pega o idToken da resposta
+    final Map<String, dynamic> responseData = json.decode(response.body);
+    return responseData['idToken'];  // Retorna o idToken
+  } else {
+    // Caso contrário, imprime a mensagem de erro
+    print('Erro ao autenticar: ${response.statusCode}');
     return null;
   }
 }
 
-Future<bool> verifyIdToken(String idToken, String apiKey) async {  // aqui é um get
-  final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyCHDutXuKV_39WzRs9dvt0VN79iREvIjnI');
 
-  // Faz a requisição POST com o ID Token
+
+Future<bool> verifyIdToken(String idToken, String apiKey) async {
+  final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDXMaZaGsPa3ajobec1c9FK0G-yCqSTMDE');
+
+  // Faz a requisição POST com o idToken
   final response = await http.post(
     url,
     body: json.encode({
@@ -34,12 +48,15 @@ Future<bool> verifyIdToken(String idToken, String apiKey) async {  // aqui é um
   );
 
   if (response.statusCode == 200) {
-    // Sucesso: A resposta é válida
+    // Se o ID token for válido
     print('ID Token verificado com sucesso!');
     return true;
   } else {
-    // Falha: ID Token inválido
+    // Se o ID token for inválido
     print('Falha na verificação do ID Token: ${response.statusCode}');
     return false;
   }
 }
+
+
+
